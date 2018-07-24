2aef805b6c1126264d38f6e79b2d2afffd9037da
Naomi Dennis <njdennis94@gmail.com>
2018-07-24 16:21:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code in this file!
 'use strict'
 function scuberGreetingForFeet(length){
-  (length <= 400 ) ? return 'This one is on me!' : (length > 2000 && length < 2500) ? return 'I will gladly take your thirty bucks.' : return 'No can do.'
+  (length <= 400 ) ? {return 'This one is on me!' }:{ (length > 2000 && length < 2500) ?{ return 'I will gladly take your thirty bucks.'} : {return 'No can do.' }} 
 }
