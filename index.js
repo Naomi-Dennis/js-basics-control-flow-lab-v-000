195275b56fb7ec097eb16d22f1192d81c426fd2f
Naomi Dennis <njdennis94@gmail.com>
2018-07-24 16:24:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,3 +24,15 @@ function ternaryCheckCity(city){
     return 'No go.'
   }
 }
+
+function switchOnCharmFromTip(tip){
+  if(tip == 'generous'){
+    return 'Thank you so much.'
+  }
+  else if(tip == 'not as generous'){
+    return 'Thank you.'
+  }
+  else if(tip == 'thanks for everything'){
+    return 'Bye.'
+  }
+}
